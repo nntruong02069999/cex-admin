61c3faca093417ade27c082d33549192e334a1e0
Nam Truong <nntruong.hvktqs@gmail.com>
2025-08-04 00:25:59 +0700

    Add down and up SVG icons for CurrentGameStatistics component; update image sources in the component to use new icons and enhance styling for better layout.

--- src/components/WingoGame/CurrentGameStatistics.tsx
+++ src/components/WingoGame/CurrentGameStatistics.tsx
@@ -1,6 +1,8 @@
 import { get } from "lodash";
-import { ARRAY_UNIT_DIGITS, CURRENCY } from "@src/constants/constants";
+import { CURRENCY } from "@src/constants/constants";
 import { GameCompletedRound } from "@src/interfaces/WingoGame";
+import upIcon from "@src/assets/images/up.svg";
+import downIcon from "@src/assets/images/down.svg";
 
 interface CurrentGameStatisticsProps {
   currentGameInfos: Partial<GameCompletedRound>;
@@ -16,25 +18,34 @@ const CurrentGameStatistics: React.FC<CurrentGameStatisticsProps> = (
       <div className="color-bets">
         <div className="color-row">
           <div className="bet-label">Phiên</div>
-          <div className="bet-amount">
-            {currentGameInfos?.issueNumber}
-          </div>
+          <div className="bet-amount">{currentGameInfos?.issueNumber}</div>
         </div>
         <div className="color-row">
           <div className="color-label">Đã chọn</div>
           <div
-            style={{ backgroundColor: "rgb(49, 204, 101)", color: "white", display: "flex", alignItems: "center", gap: "5px" }}
+            style={{
+              backgroundColor: "rgb(49, 204, 101)",
+              color: "white",
+              display: "flex",
+              alignItems: "center",
+              gap: "5px",
+            }}
             className="color-green"
-
           >
-            <img alt="buy" src="https://fiboex.net/static/media/greenup.c951824017a7b11c0dda86fb784f0e6f.svg"></  img>
+            <img alt="buy" src={upIcon}></img>
             Mua
           </div>
           <div
-            style={{ backgroundColor: "rgb(209, 47, 36)", color: "white", display: "flex", alignItems: "center", gap: "5px" }}
+            style={{
+              backgroundColor: "rgb(209, 47, 36)",
+              color: "white",
+              display: "flex",
+              alignItems: "center",
+              gap: "5px",
+            }}
             className="color-pink"
           >
-            <img alt="sell" src="https://fiboex.net/static/media/reddown.4e688600b1135732f6a23700e439250f.svg"></img>
+            <img alt="sell" src={downIcon}></img>
             Bán
           </div>
         </div>
